35870ef09c590cf5ba5021637c903095eb9402b1
MrConorAE <26195325+MrConorAE@users.noreply.github.com>
2019-06-27 12:30:59 +1200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ function reloadBtn() {
 	setTimeout(function () {
 		window.location.reload();
 	}, 1500);
-	var icon = document.getElementById("reloadIcon");
-	icon.className = "fa fa-redo-alt spinning";
 	var btn = document.getElementById("reload");
 	btn.className = "loading";
 	btn.innerHTML = "<i class='fa fa-redo-alt spinning'></i> refreshing";
